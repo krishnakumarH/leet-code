26317d4ed4f4243bbcb3f58fb8e9c2da148ffcad
Krishna kumar <krishnakumar50621@gmail.com>
2026-04-07 23:33:52 +0530

    Add time complexity comment for result allocation
    Added comment indicating time complexity of the allocation.

--- 1_two_sum_array.c
+++ 1_two_sum_array.c
@@ -3,7 +3,7 @@
  */
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
 
-   int* result=(int*)malloc(2*sizeof(int));
+   int* result=(int*)malloc(2*sizeof(int)); //o(n2)
     for(int i=0;i<numsSize;i++){
         for(int j=i+1;j<numsSize;j++){
             if(nums[i]+nums[j]==target){
